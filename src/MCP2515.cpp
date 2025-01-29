f0647de710dcebb611ae93513cf719a02fa06254
jetpax <jetpax@users.noreply.github.com>
2025-01-29 14:56:04 -0800

    add MCP2515 GP output on RXnBF pins

--- src/MCP2515.cpp
+++ src/MCP2515.cpp
@@ -28,23 +28,23 @@
 //#include "pin_manager.h"
 
 //Defines for chip select
-#define MCP2515_CS_HIGH()   DigIo::mcp_cs.Set();;
-#define MCP2515_CS_LOW()    DigIo::mcp_cs.Clear();;
-#define SPI_CAN                 SPI2
-#define SPI_TIMEOUT             10
+#define MCP2515_CS_HIGH()   GPIO_BSRR(CAN3_CS_PORT) = CAN3_CS_PIN;
+#define MCP2515_CS_LOW()    GPIO_BSRR(CAN3_CS_PORT) = (CAN3_CS_PIN << 16);
+
+#define SPI_TIMEOUT        10
 
 //Static variables
-static uint8_t readDummy;
+static uint8_t readDummy= 0x00;
 static uint8_t writeDummy = 0x00;
 
 //Set CAN controller to config mode
 void MCP2515_Initialize(void)
 {
-   //MCP2515_CS_HIGH();
-   spi_enable(SPI2);
+   MCP2515_CS_HIGH();
+   spi_enable(SPI_CAN);
 }
 
-//Set CAN contoller to config mode
+//Set CAN controller to config mode
 void MCP2515_SetTo_ConfigMode(void)
 {
 
@@ -94,7 +94,7 @@ void MCP2515_SetTo_Sleep_Mode(void)
 void MCP2515_Reset(void)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_RESET);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_RESET);
    MCP2515_CS_HIGH();
 }
 
@@ -104,9 +104,9 @@ uint8_t MCP2515_Read_Byte (uint8_t readAddress)
    uint8_t addressData;
 
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_READ);
-   readDummy = spi_xfer(SPI2,readAddress);
-   addressData = spi_xfer(SPI2,writeDummy);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_READ);
+   readDummy = spi_xfer(SPI_CAN,readAddress);
+   addressData = spi_xfer(SPI_CAN,writeDummy);
    MCP2515_CS_HIGH();
 
    return addressData;
@@ -118,8 +118,8 @@ uint8_t MCP2515_Read_RxBuffer(uint8_t readRxBuffInst)
    uint8_t rxBufferData;
 
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,readRxBuffInst);
-   rxBufferData = spi_xfer(SPI2,writeDummy);
+   readDummy = spi_xfer(SPI_CAN,readRxBuffInst);
+   rxBufferData = spi_xfer(SPI_CAN,writeDummy);
    MCP2515_CS_HIGH();
 
    return rxBufferData;
@@ -131,10 +131,10 @@ uint8_t MCP2515_Read_RxBuffer(uint8_t readRxBuffInst)
 void MCP2515_Read_RxbSequence(uint8_t readRxBuffInst, uint8_t rxLength, uint8_t *rxData)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,readRxBuffInst);
+   readDummy = spi_xfer(SPI_CAN,readRxBuffInst);
    for(uint8_t i = 0; i < rxLength; i++)
    {
-      *(rxData++) = spi_xfer(SPI2,writeDummy);
+      *(rxData++) = spi_xfer(SPI_CAN,writeDummy);
    }
    MCP2515_CS_HIGH();
 }
@@ -145,9 +145,9 @@ void MCP2515_Read_RxbSequence(uint8_t readRxBuffInst, uint8_t rxLength, uint8_t 
 void MCP2515_Write_Byte (uint8_t writeAddress, uint8_t writeData)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_WRITE);
-   readDummy = spi_xfer(SPI2,writeAddress);
-   readDummy = spi_xfer(SPI2,writeData);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_WRITE);
+   readDummy = spi_xfer(SPI_CAN,writeAddress);
+   readDummy = spi_xfer(SPI_CAN,writeData);
    MCP2515_CS_HIGH();
 }
 
@@ -158,11 +158,11 @@ void MCP2515_Write_Byte (uint8_t writeAddress, uint8_t writeData)
 void MCP2515_Write_ByteSequence (uint8_t startAddress, uint8_t endAddress, uint8_t *data)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_WRITE);
-   readDummy = spi_xfer(SPI2,startAddress);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_WRITE);
+   readDummy = spi_xfer(SPI_CAN,startAddress);
    do
    {
-      readDummy = spi_xfer(SPI2,*(data++));
+      readDummy = spi_xfer(SPI_CAN,*(data++));
    }
    while(startAddress++ != endAddress);
    MCP2515_CS_HIGH();
@@ -176,15 +176,15 @@ void MCP2515_Write_ByteSequence (uint8_t startAddress, uint8_t endAddress, uint8
 void MCP2515_Load_TxSequence(uint8_t loadtxBnSidhInst, uint8_t *idReg, uint8_t dlc, uint8_t *txData)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,loadtxBnSidhInst);
+   readDummy = spi_xfer(SPI_CAN,loadtxBnSidhInst);
    for(uint8_t i = 0; i < 4; i++)
    {
-      readDummy = spi_xfer(SPI2,*(idReg++));      // Write id from the four id registers
+      readDummy = spi_xfer(SPI_CAN,*(idReg++));      // Write id from the four id registers
    }
-   readDummy = spi_xfer(SPI2,dlc);
+   readDummy = spi_xfer(SPI_CAN,dlc);
    for(uint8_t i = 0; i < 8; i++)
    {
-      readDummy = spi_xfer(SPI2,*(txData++));     // Write 8 data bytes
+      readDummy = spi_xfer(SPI_CAN,*(txData++));     // Write 8 data bytes
    }
    MCP2515_CS_HIGH();
 }
@@ -195,8 +195,8 @@ void MCP2515_Load_TxSequence(uint8_t loadtxBnSidhInst, uint8_t *idReg, uint8_t d
 void MCP2515_Load_TxBuffer (uint8_t loadTxBuffInst, uint8_t txBufferData)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,loadTxBuffInst);
-   readDummy = spi_xfer(SPI2,txBufferData);
+   readDummy = spi_xfer(SPI_CAN,loadTxBuffInst);
+   readDummy = spi_xfer(SPI_CAN,txBufferData);
    MCP2515_CS_HIGH();
 }
 
@@ -204,7 +204,7 @@ void MCP2515_Load_TxBuffer (uint8_t loadTxBuffInst, uint8_t txBufferData)
 void MCP2515_RequestToSend (uint8_t rtsTxBuffInst)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,rtsTxBuffInst);
+   readDummy = spi_xfer(SPI_CAN,rtsTxBuffInst);
    MCP2515_CS_HIGH();
 }
 
@@ -214,8 +214,8 @@ uint8_t MCP2515_Read_Status (void)
    uint8_t ctrlStat;
 
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_READ_STATUS);
-   ctrlStat = spi_xfer(SPI2,writeDummy);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_READ_STATUS);
+   ctrlStat = spi_xfer(SPI_CAN,writeDummy);
    MCP2515_CS_HIGH();
 
    return ctrlStat;
@@ -227,8 +227,8 @@ uint8_t MCP2515_Get_RxStatus (void)
    uint8_t rxStatus;
 
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_RX_STATUS);
-   rxStatus = spi_xfer(SPI2,writeDummy);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_RX_STATUS);
+   rxStatus = spi_xfer(SPI_CAN,writeDummy);
    MCP2515_CS_HIGH();
 
    return rxStatus;
@@ -239,9 +239,43 @@ uint8_t MCP2515_Get_RxStatus (void)
 void MCP2515_Bit_Modify (uint8_t regAddress, uint8_t maskByte, uint8_t dataByte)
 {
    MCP2515_CS_LOW();
-   readDummy = spi_xfer(SPI2,MCP2515_BIT_MOD);
-   readDummy = spi_xfer(SPI2,regAddress);
-   readDummy = spi_xfer(SPI2,maskByte);
-   readDummy = spi_xfer(SPI2,dataByte);
+   readDummy = spi_xfer(SPI_CAN,MCP2515_BIT_MOD);
+   readDummy = spi_xfer(SPI_CAN,regAddress);
+   readDummy = spi_xfer(SPI_CAN,maskByte);
+   readDummy = spi_xfer(SPI_CAN,dataByte);
    MCP2515_CS_HIGH();
+}
+
+// From MCP2515 Datasheet
+// --------------------------------------------------
+// RX0BF and RX1BF Pins CONFIGURED AS DIGITAL OUTPUT
+// --------------------------------------------------
+// When used as digital outputs, the BnBFM bits
+// (BFPCTRL[1:0]) must be cleared and the BnBFE bits
+// (BFPCTRL[3:2]) must be set for the associated buffer.
+// In this mode, the state of the pin is controlled by the
+// BnBFS bits (BFPCTRL[5:4]), and should be modified only 
+// by using the SPI BIT MODIFY command to prevent glitches
+// from occurring on either of the buffer full pins.
+// The RXnBF pins can be disabled to the high-impedance
+// state by clearing the BnBFE bits (BFPCTRL[3:2]).
+
+
+// Enable RXBn pin, or set to HiZ
+void MCP2515_PinEn(uint8_t pin, bool enable) {
+    uint8_t BnFMbit = (pin == 0) ?  BFPCTRL_B0BFM:BFPCTRL_B1BFM ;
+    uint8_t BnFEbit = (pin == 0) ?  BFPCTRL_B0BFE:BFPCTRL_B1BFE ;
+
+    if (enable) {
+        MCP2515_Bit_Modify(MCP2515_BFPCTRL, BnFMbit, 0x00);
+        MCP2515_Bit_Modify(MCP2515_BFPCTRL, BnFEbit, 0xFF);
+    } else {
+        MCP2515_Bit_Modify(MCP2515_BFPCTRL, BnFEbit, 0x00);
+    }
+}
+
+// set/clear an enabled RXBn output pin 
+void MCP2515_Out_Pin(uint8_t pin, bool state) {
+   uint8_t BnBFSbit = (pin == 0) ? BFPCTRL_B0BFS : BFPCTRL_B1BFS;
+   MCP2515_Bit_Modify(MCP2515_BFPCTRL, BnBFSbit, state ? 0xFF : 0x00);
 }
